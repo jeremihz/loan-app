bbb27dd2f596c63af85859f57a62e64998e595d2
jeremihz <mihjere68@gmail.com>
2019-08-28 12:15:47 +0300

    added database

---
+++ database/migrations/2019_05_25_192553_create_loan_applications_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLoanApplicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('loan_applications', function (Blueprint $table) {
+            $table->bigIncrements('id')->unique();
+            $table->string('employmentstatus');
+            $table->string('occupation');
+            $table->string('BusinessName');
+            $table->string('BusinessAddress');
+            $table->integer('phoneNumber');
+            $table->integer('loanAmount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('loan_applications');
+    }
+}
